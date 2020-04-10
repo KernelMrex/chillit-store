b4ba0e48f8fcd9cd52694d6b29849f1b45fd9dad
KernelMrex <24671528+KernelMrex@users.noreply.github.com>
2020-04-11 01:29:19 +0300

    Added tests for 'places/server'(formatting)

--- places/server_test.go
+++ places/server_test.go
@@ -53,8 +53,8 @@ func TestAddPlaceRequest(t *testing.T) {
 	for i := 0; i < 6; i++ {
 		if resp, err := mockClient.AddPlace(context.Background(), &AddPlaceRequest{
 			Place: &Place{
-				Title: fmt.Sprintf("Test title %d", i),
-				Address: fmt.Sprintf("Test address %d", i),
+				Title:       fmt.Sprintf("Test title %d", i),
+				Address:     fmt.Sprintf("Test address %d", i),
 				Description: fmt.Sprintf("Test description %d", i),
 			},
 		}); err != nil || resp.Id != uint64(i) {
@@ -76,10 +76,10 @@ func TestGetPlacesRequest(t *testing.T) {
 		return
 	}
 	for i, place := range resp.Places {
-		if place.Id != uint64(i + offset) ||
-			place.Title != fmt.Sprintf("Test title %d", i + offset) ||
-			place.Address != fmt.Sprintf("Test address %d", i + offset) ||
-			place.Description != fmt.Sprintf("Test description %d", i + offset) {
+		if place.Id != uint64(i+offset) ||
+			place.Title != fmt.Sprintf("Test title %d", i+offset) ||
+			place.Address != fmt.Sprintf("Test address %d", i+offset) ||
+			place.Description != fmt.Sprintf("Test description %d", i+offset) {
 			t.Fail()
 			return
 		}
