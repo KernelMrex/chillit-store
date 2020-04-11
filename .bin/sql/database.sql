c88b24e52bede704fdd3078c7dc5e751ae13ff49
KernelMrex <24671528+KernelMrex@users.noreply.github.com>
2020-04-11 19:09:31 +0300

    Added tests for 'models.MysqlDB'

--- .bin/sql/database.sql
+++ .bin/sql/database.sql
@@ -2,10 +2,10 @@ CREATE TABLE IF NOT EXISTS `place`
 (
     `id`          BIGINT NOT NULL AUTO_INCREMENT,
     `title`       VARCHAR(64) NOT NULL,
-    `address`     VARCHAR(64),
-    `description` TEXT,
+    `address`     VARCHAR(64) NOT NULL ,
+    `description` TEXT NOT NULL,
     PRIMARY KEY (`id`)
 );
 
-DROP TABLE `place`;
+#DROP TABLE `place`;
 
